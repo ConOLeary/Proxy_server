5d8b09b4f0a34f9d83c9fa7ba623d63e66895461
Con <conolaoghaire@gmail.com>
2020-02-23 20:19:37 +0000

    Client<->Socket GET
    Client can now send a URL to the "server", which uses a GET request to pull contents at the URL from an actual web server.
    Wil focus on making proxy server operational before expanding the range of functionality the client class can avail of.

--- server2.py
+++ server2.py
@@ -1,12 +1,26 @@
 # import socket programming library 
-import socket 
+import socket
   
 # import thread module 
 from _thread import *
 import threading 
-  
+
+import requests
+
+import atexit
+
 print_lock = threading.Lock() 
-  
+
+def release_port():
+    socket.close()
+
+def GET_request(url):
+    response = requests.get(url)
+    print('GET response: ', response.status_code)
+    response = str(response)
+    response_bytes = response.encode()
+    return response_bytes
+
 # thread function 
 def threaded(c): 
     while True: 
@@ -19,12 +33,15 @@ def threaded(c):
             # lock released on exit 
             print_lock.release() 
             break
-  
+        
+        http_response = GET_request(data)
+        print('about to send: ',http_response)
         # reverse the given string from client 
-        data = data[::-1] 
+        #data = data[::-1] 
   
         # send back reversed string to client 
-        c.send(data) 
+        #c.send(data) 
+        c.send(http_response)
   
     # connection closed 
     c.close() 
@@ -36,9 +53,10 @@ def Main():
     # reverse a port on your computer 
     # in our case it is 12345 but it 
     # can be anything 
-    port = 12345
+    port = 12348
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.bind((host, port)) 
+    atexit.register(release_port, s)
     print("socket bound to port", port) 
   
     # put the socket into listening mode 
@@ -47,7 +65,7 @@ def Main():
   
     # a forever loop until client wants to exit 
     while True: 
-  
+        
         # establish connection with client 
         c, addr = s.accept() 
   
